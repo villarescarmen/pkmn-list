11fb0d67e053c97f1b72818510c19d6a556104ab
villarescarmenfp <villarescarmenfp@gmail.com>
2024-09-04 11:28:05 +0200

    relleno

---
+++ script.js
@@ -0,0 +1,30 @@
+// script.js
+const pokemonList = [
+    "Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise",
+    "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata",
+    "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran♀","Nidoran hembra", "Nidorina", "Nidoqueen", "Nidoran♂", "Nidoran macho", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetch'd", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby","Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "Mr. Mime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini","Dragonair", "Dragonite", "Mewtwo", "Mew"
+];
+
+document.getElementById('pokemon-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const inputName = document.getElementById('pokemon-name').value.trim();
+    const resultDiv = document.getElementById('result');
+    
+    if (inputName) {
+        const isPokemonFirstGen = pokemonList.some(pokemon => pokemon.toLowerCase() === inputName.toLowerCase());
+        if (isPokemonFirstGen) {
+            resultDiv.textContent = `${inputName} es un Pokémon de 1ª generación.`;
+            resultDiv.style.color = 'green';
+        } else {
+            resultDiv.textContent = `${inputName} no es un Pokémon de 1ª generación, o puede que no está bien escrito.`;
+            resultDiv.style.color = 'red';
+        }
+    } else {
+        resultDiv.textContent = 'Por favor, ingresa un nombre de Pokémon de primera Generación (nº 1 al 151).';
+        resultDiv.style.color = 'orange';
+    }
+
+    
+});
+
+
